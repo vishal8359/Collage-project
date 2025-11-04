70d33b5510b29f3150c89d2c0871f378f019a842
Vishal <vishalgupta8359@gmail.com>
2025-11-04 22:43:02 +0530

    Initialise Sangam, install dependencies, folder structuring and collaborations

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AppContextProvider } from './context/AppContext';
+import './index.css';
+// import { GoogleOAuthProvider } from '@react-oauth/google'; 
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>  */}
+      <BrowserRouter>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </BrowserRouter>
+    {/* </GoogleOAuthProvider> */}
+  </React.StrictMode>
+);
